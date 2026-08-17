94eea54633b39a87679092f58095d52cd66639df
nozyx12 <nozyxoff@proton.me>
2026-08-17 20:31:28 +0200

    v1.0.1: Added StriderLoader v1.1.0

--- src/main/java/dev/nozyx/strider/installer/InstallerConstants.java
+++ src/main/java/dev/nozyx/strider/installer/InstallerConstants.java
@@ -24,9 +24,15 @@ public final class InstallerConstants {
 
     private InstallerConstants() {}
 
-    public static final String VERSION = "1.0.0";
+    public static final String VERSION = "1.0.1";
 
     public static final Map<String, String[]> SUPPORTED_VERSIONS = Map.of(
+            "1.1.0", new String[]{
+                    "26.2",
+                    "26.1.2",
+                    "26.1.1",
+                    "26.1"
+            },
             "1.0.0", new String[]{
                     "26.2",
                     "26.1.2",
